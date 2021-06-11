f10e3433c7a7d4b5e7837c4c4d1bd9aadb385302
David Dias <thedaviddias@gmail.com>
2021-06-10 22:44:15 -0400

    feat: add prettier config

---
+++ packages/prettier-config/src/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // Include parentheses around a sole arrow function parameter
+  arrowParens: "avoid",
+  // Specify the line length that the printer will wrap on
+  printWidth: 100,
+  // Print semicolons at the ends of statements
+  semi: false,
+  // Use single quotes instead of double quotes
+  singleQuote: true,
+};
